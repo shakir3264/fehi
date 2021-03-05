2166eea22d72de859a817e1c63661569c3b7c252
Shakir <shakir19@gmail.com>
2021-03-05 22:22:40 +0500

    working for all models, but single guest per reservations for now

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Guest, Companies
+from .models import Guest, Companies, Reservations, Invoice, Guesthouse, Rooms
 
 
 class GuestAdmin(admin.ModelAdmin):
@@ -8,5 +8,12 @@ class GuestAdmin(admin.ModelAdmin):
 
 
 
+
+
 admin.site.register(Guest, GuestAdmin)
+
 admin.site.register(Companies)
+admin.site.register(Rooms)
+admin.site.register(Reservations)
+admin.site.register(Invoice)
+admin.site.register(Guesthouse)
--- reservations/models.py
+++ reservations/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Companies(models.Model):
@@ -6,6 +7,14 @@ class Companies(models.Model):
 
     def __str__(self):
         return f"{self.company}"
+
+
+class Rooms(models.Model):
+    room_no = models.CharField(max_length=20)
+    room_type = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.room_no}"
 
 
 
@@ -16,9 +25,42 @@ class Guest(models.Model):
     nationality = models.CharField(max_length=40)
     phone = models.DecimalField(max_digits=15, decimal_places=0)
     category = models.CharField(max_length=40)
-    company = models.ForeignKey('Companies', on_delete=models.CASCADE, null=True, blank=True)
+    company = models.ForeignKey('Companies', on_delete=models.PROTECT, null=True, blank=True)
 
     def __str__(self):
         return f"{self.name} {self.email} {self.passport} {self.nationality}"
 
 
+
+class Reservations(models.Model):
+    guest = models.ForeignKey('Guest', on_delete=models.RESTRICT, null=False)
+    room = models.ForeignKey('Rooms', on_delete=models.RESTRICT, null=False)
+    checkin_date = models.DateTimeField(default=timezone.now)
+    checkout_date = models.DateTimeField(default=timezone.now)
+    booking_type = models.CharField(max_length=40)
+    meal_plan = models.CharField(max_length=40)
+    create_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.guest} {self.room}"
+
+
+class Invoice(models.Model):
+    invoice_no = models.CharField(max_length=10)
+    reservation = models.ForeignKey('Reservations', related_name="reservations", on_delete=models.RESTRICT, null=False)
+    daily_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    other_charges = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_method = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.invoice_no}"
+
+
+class Guesthouse(models.Model):
+    name = models.CharField(max_length=20)
+    capacity = models.DecimalField(max_digits=15, decimal_places=0)
+    address = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.name}"
